83fba1d11b7806759107ef4889655bb296a08139
jackboyman01 <30963391+jackboyman01@users.noreply.github.com>
2020-09-01 21:14:21 +0200

    update without bugs

--- Menu/Options.gd
+++ Menu/Options.gd
@@ -10,6 +10,7 @@ func _on_CheckBox_pressed():
 	OS.window_fullscreen = !OS.window_fullscreen
 
 func _on_FadeIn_fade_finished():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Menu/Main Menu.tscn")
 
 func _ready():
--- Menu/title_screen.gd
+++ Menu/title_screen.gd
@@ -15,6 +15,7 @@ func _on_Button_pressed(scene_to_load):
 	$FadeIn.fade_in()
 
 func _on_FadeIn_fade_finished():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene(scene_path_to_load)
 
 func _process(_delta):
