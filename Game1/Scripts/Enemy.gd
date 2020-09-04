432aee5fe9fc3c2bc2fa41bcc454741e349dc954
jackboyman01 <30963391+jackboyman01@users.noreply.github.com>
2020-09-04 20:17:50 +0200

    enemy follow and points

---
+++ Game1/Scripts/Enemy.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+signal enemy_hit
+
+var max_speed = 300
+var motion = Vector2()
+onready var obj = get_parent().get_node("Player")
+
+func _physics_process(delta):
+	var dir = (obj.global_position - global_position).normalized()
+# warning-ignore:return_value_discarded
+	move_and_collide(dir * max_speed * delta)
+
+func _ready():
+# warning-ignore:return_value_discarded
+	connect("enemy_hit", get_parent(), "_on_Enemy_enemy_hit")
+
+func _on_Player_Detection_body_entered(body):
+	if body.is_in_group("player"):
+		emit_signal("enemy_hit")
--- Game1/Scripts/World.gd
+++ Game1/Scripts/World.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+var points = 0
+
 func _process(_delta):
+	get_node("Points_Counter").set_text("Score: " + str(points))
 	if Input.is_action_pressed("key_exit"):
 		$FadeIn.show()
 		$FadeIn.fade_in()
@@ -8,3 +11,27 @@ func _process(_delta):
 func _on_FadeIn_fade_finished():
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Menu/Main Menu/Main Menu.tscn")
+
+func _on_Enemy_enemy_hit():
+# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
+
+func spawn_enemy():
+	var rand = RandomNumberGenerator.new()
+	var enemyscene = load("res://Game1/Mini Scenes/Enemy.tscn")
+	var screen_size = get_viewport().get_visible_rect().size
+	var enemy = enemyscene.instance()
+	rand.randomize()
+	var x = rand.randf_range(0,screen_size.x)
+	rand.randomize()
+	var y = rand.randf_range(0,screen_size.y)
+	enemy.position.y = y
+	enemy.position.x = x
+	add_child(enemy)
+
+
+func _on_Timer_timeout():
+	spawn_enemy()
+
+func _on_Points_timeout():
+	points += 5
