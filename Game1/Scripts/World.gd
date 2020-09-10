a8260a7d02446c2fc663a634edd3ca04bbd8ba1f
jackboyman01 <30963391+jackboyman01@users.noreply.github.com>
2020-09-10 16:04:48 +0200

    game3 + game4

--- Game1/Scripts/World.gd
+++ Game1/Scripts/World.gd
@@ -3,7 +3,7 @@ extends Node2D
 var points = 0
 
 func _process(_delta):
-	get_node("Points_Counter").set_text("Score: " + str(points))
+	get_node("CanvasLayer/Points_Counter").set_text("Score: " + str(points))
 	if points > SaveHighscore.read_savegame():
 		SaveHighscore.save(points)
 	if Input.is_action_pressed("key_exit"):
